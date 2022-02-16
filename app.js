67b023c40ca9bc206f65441f8913f2d6d24bbe18
Babak Arjomand <babak@Babaks-MacBook-Air.local>
2022-02-16 17:34:01 +0000

    get api users #21

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const {
+  getTopics,
+  getArticlesById,
+  patchArticle,
+  getUsers,
+} = require("./controller");
+const {
+  handlePSQLerrors,
+  customErrors,
+  handle500s,
+} = require("./error-handling");
+
+app.use(express.json());
+
+app.get("/api/topics", getTopics);
+app.get("/api/articles/:article_id", getArticlesById);
+app.patch("/api/articles/:article_id", patchArticle);
+app.get("/api/users", getUsers);
+
+app.all("/*", (req, res) => {
+  res.status(404).send({ message: "path not found" });
+});
+
+app.use(handlePSQLerrors);
+app.use(customErrors);
+app.use(handle500s);
+
+module.exports = app;
---
+++ controller.js
@@ -0,0 +1,43 @@
+const {
+  selectTopics,
+  selectArticlesById,
+  increaseArticleVote,
+  selectUsers,
+} = require("./model");
+
+exports.getTopics = (req, res, next) => {
+  selectTopics()
+    .then((topics) => {
+      res.status(200).send({ topics });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getArticlesById = (req, res, next) => {
+  const id = req.params.article_id;
+  selectArticlesById(id)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch(next);
+};
+
+exports.patchArticle = (req, res, next) => {
+  const id = req.params.article_id;
+  const newVote = req.body.inc_votes;
+  increaseArticleVote(id, newVote)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch(next);
+};
+
+exports.getUsers = (req, res, next) => {
+  selectUsers()
+    .then((users) => {
+      res.status(200).send({ users });
+    })
+    .catch(next);
+};
